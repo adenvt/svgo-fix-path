f8c27504b4df247b9ef7248fc1bd1366aa4ad081
adenvt <adenvt@users.noreply.github.com>
2025-05-08 19:55:45 +0000

    deploy: c14ad5de686f8241bc4a0ccf16842808747a28cc

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './fix-path'
-export * from './svgo-plugin'
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [tailwindcss()],
-  build  : { outDir: './demo/dist' },
-  test   : { coverage: { all: false } },
-})
